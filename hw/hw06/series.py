802aced5bcd263494abf491def4b7474bbe914ba
Kevin O'Brien <irishpadres@gmail.com>
2015-06-22 19:25:30 -0700

    Lucas initial checkin

--- hw/hw06/series.py
+++ hw/hw06/series.py
@@ -2,12 +2,23 @@
 
 
 def fibonacci(n):
-    sequence = list(range(0, n))
-    print(sequence)
+    """Generate nth position of Fibonacci sequence"""
+    sequence = [0, 1]
+    sequence.extend([0] * (n - 2))
     if (n > 2):
         for i in range(2, n):
             sequence[i] = sequence[i - 2] + sequence[i - 1]
-            print(i, sequence)
+    return sequence[n-1]
+
+
+def lucas(n):
+    """Generate nth position of Lucas number series"""
+    sequence = [2, 1]
+    sequence.extend([0] * (n - 2))
+    if (n > 2):
+        for i in range(2, n):
+            sequence[i] = sequence[i - 2] + sequence[i - 1]
     return sequence[n-1]
 
 print(fibonacci(7))
+print(lucas(6))
