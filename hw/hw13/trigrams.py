636a2e5679d4d11ea09ea88fb7fbe4ca685e043b
Kevin O'Brien <irishpadres@gmail.com>
2015-07-06 13:32:51 -0700

    better cleanup of string

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -10,19 +10,26 @@ def readFile(filename):
     fh = open(filename, 'r')
     lines = fh.readlines()
     for line in lines:
-        strpdLine = line.rstrip()
-        if (len(strpdLine) > 50 or re.search("\.$", strpdLine)):
-            print(strpdLine)
-        # Split lines on letters, numbers, and apostrophes
-        # Hypenated words will be split
-        wordList = re.split("[^\w']+", strpdLine)
-        # Shortening length by one so that we don't overrun the index
-        # Also, this is preferred since we only want word pairs
-        for i in range(len(wordList) - 1):
-            curWord = wordList[i]
-            nextWord = wordList[i + 1]
-            # print("{0:d}: {1} -> {2}".format(i, curWord, nextWord))
-            # wordDict[(curWord, nextWord)]
+        # Treat double hyphens as a space
+        cleanLine = line.rstrip().replace("--", " ")
+        if (len(cleanLine) > 50 or re.search("\.$", cleanLine)):
+            print(cleanLine)
+            buildDict(cleanLine)
+
+
+def buildDict(line):
+    # Split lines on letters, numbers, and apostrophes
+    # Hypenated words will be split
+    wordList = re.split("[^\w']+", line)
+    # Shortening length by one so that we don't overrun the index
+    # Also, this is preferred since we only want to evaluate the final
+    # three words
+    for i in range(len(wordList) - 2):
+        curWord = wordList[i]
+        nextWord = wordList[i + 1]
+        lastWord = wordList[i + 2]
+        # print("{0:d}: {1} -> {2}".format(i, curWord, nextWord))
+        # wordDict[(curWord, nextWord)]
 
 
 # Start main
