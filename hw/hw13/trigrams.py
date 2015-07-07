2522075be72a4ed3c93f2ab312c11a02dcee0026
Kevin O'Brien <irishpadres@gmail.com>
2015-07-06 20:15:49 -0700

    fixed error in while loop

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -5,10 +5,34 @@ import sys
 import random
 
 
+sampleText = [
+    "One night--it was on the twentieth of March, 1888--I was",
+    "returning from a journey to a patient (for I had now returned to",
+    "civil practice), when my way led me through Baker Street. As I",
+    "passed the well-remembered door, which must always be associated",
+    "in my mind with my wooing, and with the dark incidents of the",
+    "Study in Scarlet, I was seized with a keen desire to see Holmes",
+    "again, and to know how he was employing his extraordinary powers.",
+    "His rooms were brilliantly lit, and, even as I looked up, I saw",
+    "his tall, spare figure pass twice in a dark silhouette against",
+    "the blind. He was pacing the room swiftly, eagerly, with his head",
+    "sunk upon his chest and his hands clasped behind him. To me, who",
+    "knew his every mood and habit, his attitude and manner told their",
+    "own story. He was at work again. He had risen out of his",
+    "drug-created dreams and was hot upon the scent of some new",
+    "problem. I rang the bell and was shown up to the chamber which",
+    "had formerly been in part my own."
+    ]
+
+
 def readFile(filename):
-    fullText = []
     fh = open(filename, 'r')
     lines = fh.readlines()
+    return lines
+
+
+def cleanText(lines):
+    fullText = []
     for line in lines:
         # Look for lines that either long enough to be part of a paragraph or
         # end in a period.
@@ -18,7 +42,6 @@ def readFile(filename):
             # Split lines on letters, numbers, hyphens and apostrophes
             wordList = re.split("[^\w'-]+", cleanLine)
             fullText.extend(filter(None, wordList))
-
     return(fullText)
 
 
@@ -39,17 +62,17 @@ def buildDict(wordList):
 
 
 def writeStory(wordDict):
-    wordTuple = random.choice(list(wordDict.keys()))
-    # Create 6 to 8 paragraphs
-    for paraNum in range(0, random.randrange(6, 9)):
+    # Create 3 to 4 paragraphs
+    for paraNum in range(0, random.randrange(3, 5)):
         # Create 6 to 8 sentances
         for sentNum in range(0, random.randrange(6, 9)):
-            # Create 10 to 15 words per sentance
-            for wordNum in range(0, random.randrange(10, 16)):
+            wordTuple = random.choice(list(wordDict))
+            while wordTuple in wordDict:
                 priWord = wordTuple[0]
                 secWord = wordTuple[1]
                 terWord = random.choice(wordDict[wordTuple])
-                print("{0} {1} {2}".format(priWord, secWord, terWord))
+                print("{0}, {1}: {2} {3} {4}".format(paraNum, sentNum, priWord,
+                      secWord, terWord))
                 wordTuple = (secWord, terWord)
 
 
@@ -58,10 +81,11 @@ if __name__ == '__main__':
     try:
         filename = sys.argv[1]
     except IndexError:
-        print("Need a file name")
-        sys.exit(1)
+        print("No file name given...using sample text")
+        lines = sampleText
     else:
-        text = readFile(filename)
-        wordDict = buildDict(text)
-        story = writeStory(wordDict)
-        # print(story)
+        lines = readFile(filename)
+    text = cleanText(lines)
+    wordDict = buildDict(text)
+    story = writeStory(wordDict)
+    # print(story)
