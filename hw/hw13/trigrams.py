c07dd4f90fbd35c0261e663308e0c414e12b442f
Kevin O'Brien <irishpadres@gmail.com>
2015-07-06 18:59:02 -0700

    first pass at writing the story

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -2,8 +2,7 @@
 
 import re
 import sys
-
-wordDict = {}
+import random
 
 
 def readFile(filename):
@@ -24,6 +23,7 @@ def readFile(filename):
 
 
 def buildDict(wordList):
+    wordDict = {}
     # Shortening length by two so that we don't overrun the index
     # Also, this is preferred since we only want to evaluate the final
     # three words
@@ -31,11 +31,26 @@ def buildDict(wordList):
         curWord = wordList[i]
         nextWord = wordList[i + 1]
         lastWord = wordList[i + 2]
-        if (curWord.istitle()):
-            if ((curWord.lower(), nextWord) in wordDict):
-                print("{0} and {1}".format(curWord, curWord.lower()))
+        # if (curWord.istitle()):
+        #    if ((curWord.lower(), nextWord) in wordDict):
+        #        print("{0} and {1}".format(curWord, curWord.lower()))
         wordDict.setdefault((curWord, nextWord), []).append(lastWord)
-    return(0)
+    return(wordDict)
+
+
+def writeStory(wordDict):
+    wordTuple = random.choice(list(wordDict.keys()))
+    # Create 6 to 8 paragraphs
+    for paraNum in range(0, random.randrange(6, 9)):
+        # Create 6 to 8 sentances
+        for sentNum in range(0, random.randrange(6, 9)):
+            # Create 10 to 15 words per sentance
+            for wordNum in range(0, random.randrange(10, 16)):
+                priWord = wordTuple[0]
+                secWord = wordTuple[1]
+                terWord = random.choice(wordDict[wordTuple])
+                print("{0} {1} {2}".format(priWord, secWord, terWord))
+                wordTuple = (secWord, terWord)
 
 
 # Start main
@@ -47,7 +62,6 @@ if __name__ == '__main__':
         sys.exit(1)
     else:
         text = readFile(filename)
-        buildDict(text)
-        for pair in wordDict.keys():
-            print("{0}: {1}".format(pair, wordDict[pair]))
-        # print(wordDict)
+        wordDict = buildDict(text)
+        story = writeStory(wordDict)
+        # print(story)
