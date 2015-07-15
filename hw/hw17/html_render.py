10157df43a4830bfe056603f8783148121bb3364
Kevin O'Brien <irishpadres@gmail.com>
2015-07-15 15:00:13 -0700

    step 3 working...getting easier...am I jinxing it?

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -12,24 +12,32 @@ class Element(object):
 
     BASE_INDENT = "    "
 
-    def __init__(self, tag="", content=None):
+    def __init__(self, tag="", content=None, oneLineFlag=False):
         self.tag = tag
         self.content = []
         self.append(content)
+        self.oneLineFlag = oneLineFlag
 
     def append(self, content):
         if (content):
             self.content.append(content)
 
     def render(self, fh, indent=""):
-        fh.write("%s<%s>\n" % (indent, self.tag))
+        fh.write("%s<%s>" % (indent, self.tag))
+        if not self.oneLineFlag:
+            fh.write("\n")
         curIndent = indent + Element.BASE_INDENT
         for content in self.content:
             if (isinstance(content, str)):
-                fh.write("%s%s\n" % (curIndent, content))
+                if self.oneLineFlag:
+                    fh.write("%s" % (content))
+                else:
+                    fh.write("%s%s\n" % (curIndent, content))
             elif (isinstance(content, Element)):
                 content.render(fh, curIndent)
-        fh.write("%s</%s>\n" % (indent, self.tag))
+        if not self.oneLineFlag:
+            fh.write("%s" % (indent))
+        fh.write("</%s>\n" % (self.tag))
 
 
 class Html(Element):
@@ -52,3 +60,21 @@ class P(Element):
 
     def __init__(self, content=""):
         Element.__init__(self, tag="p", content=content)
+
+
+class Head(Element):
+
+    def __init__(self, content=""):
+        Element.__init__(self, tag="head", content=content)
+
+
+class OneLineTag(Element):
+
+    def __init__(self, tag="", content=""):
+        Element.__init__(self, tag=tag, content=content, oneLineFlag=True)
+
+
+class Title(OneLineTag):
+
+    def __init__(self, content=""):
+        OneLineTag.__init__(self, tag="title", content=content)
