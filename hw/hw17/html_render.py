1217b7fad79b844079da95591207f6309bf0a7e3
Kevin O'Brien <irishpadres@gmail.com>
2015-07-15 16:19:51 -0700

    Step 4 in the can.

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -12,18 +12,25 @@ class Element(object):
 
     BASE_INDENT = "    "
 
-    def __init__(self, tag="", content=None, oneLineFlag=False):
+    def __init__(self, tag="", content=None, oneLineFlag=False, **kwargs):
         self.tag = tag
         self.content = []
         self.append(content)
         self.oneLineFlag = oneLineFlag
+        self.attrs = []
+        self.attrStr = ''
+        if (kwargs is not None):
+            for key, val in kwargs.items():
+                self.attrs.append("%s=\"%s\"" % (key, val))
+        if (len(self.attrs) > 0):
+            self.attrStr = " " + " ".join(self.attrs)
 
     def append(self, content):
         if (content):
             self.content.append(content)
 
     def render(self, fh, indent=""):
-        fh.write("%s<%s>" % (indent, self.tag))
+        fh.write("%s<%s%s>" % (indent, self.tag, self.attrStr))
         if not self.oneLineFlag:
             fh.write("\n")
         curIndent = indent + Element.BASE_INDENT
@@ -42,8 +49,8 @@ class Element(object):
 
 class Html(Element):
 
-    def __init__(self, content=""):
-        Element.__init__(self, tag="html", content=content)
+    def __init__(self, content="", **kwargs):
+        Element.__init__(self, tag="html", content=content, oneLineFlag=False, **kwargs)
 
     def render(self, fh, indent=""):
         fh.write("<!DOCTYPE html>\n")
@@ -52,29 +59,29 @@ class Html(Element):
 
 class Body(Element):
 
-    def __init__(self, content=""):
-        Element.__init__(self, tag="body", content=content)
+    def __init__(self, content="", **kwargs):
+        Element.__init__(self, tag="body", content=content, oneLineFlag=False, **kwargs)
 
 
 class P(Element):
 
-    def __init__(self, content=""):
-        Element.__init__(self, tag="p", content=content)
+    def __init__(self, content="", **kwargs):
+        Element.__init__(self, tag="p", content=content, oneLineFlag=False, **kwargs)
 
 
 class Head(Element):
 
-    def __init__(self, content=""):
-        Element.__init__(self, tag="head", content=content)
+    def __init__(self, content="", **kwargs):
+        Element.__init__(self, tag="head", content=content, oneLineFlag=False, **kwargs)
 
 
 class OneLineTag(Element):
 
-    def __init__(self, tag="", content=""):
-        Element.__init__(self, tag=tag, content=content, oneLineFlag=True)
+    def __init__(self, tag="", content="", **kwargs):
+        Element.__init__(self, tag=tag, content=content, oneLineFlag=True, **kwargs)
 
 
 class Title(OneLineTag):
 
-    def __init__(self, content=""):
-        OneLineTag.__init__(self, tag="title", content=content)
+    def __init__(self, content="", **kwargs):
+        OneLineTag.__init__(self, tag="title", content=content, **kwargs)
--- hw/hw17/run_html_render.py
+++ hw/hw17/run_html_render.py
@@ -60,25 +60,6 @@ def render(page, filename):
 # # Step 3
 # ##########
 
-page = hr.Html()
-
-head = hr.Head()
-head.append(hr.Title(u"PythonClass = Revision 1087:"))
-
-page.append(head)
-
-body = hr.Body()
-
-body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
-body.append(hr.P(u"And here is another piece of text -- you should be able to add any number"))
-
-page.append(body)
-
-render(page, u"test_html_output3.html")
-
-# # Step 4
-# ##########
-
 # page = hr.Html()
 
 # head = hr.Head()
@@ -88,12 +69,31 @@ render(page, u"test_html_output3.html")
 
 # body = hr.Body()
 
-# body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
-#               style=u"text-align: center; font-style: oblique;"))
+# body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
+# body.append(hr.P(u"And here is another piece of text -- you should be able to add any number"))
 
 # page.append(body)
 
-# render(page, u"test_html_output4.html")
+# render(page, u"test_html_output3.html")
+
+# # Step 4
+# ##########
+
+page = hr.Html()
+
+head = hr.Head()
+head.append(hr.Title(u"PythonClass = Revision 1087:"))
+
+page.append(head)
+
+body = hr.Body()
+
+body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
+               style=u"text-align: center; font-style: oblique;"))
+
+page.append(body)
+
+render(page, u"test_html_output4.html")
 
 # # Step 5
 # #########
